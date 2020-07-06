9ae58d53930f50044d07ea51bcf906f84b9ee48b
syedasif <syedasif.work@gmail.com>
2020-07-06 21:26:32 +0530

    Lab04:Python Lists, Data Abstraction  Completed

---
+++ lab/lab04/lab04.py
@@ -0,0 +1,192 @@
+from math import sqrt
+
+
+def reverse_iter(lst):
+    """Returns the reverse of the given list.
+
+    >>> reverse_iter([1, 2, 3, 4])
+    [4, 3, 2, 1]
+    """
+    "*** YOUR CODE HERE ***"
+    lst1 = []
+    for i in lst:
+        lst1 = [i] + lst1
+    return lst1
+
+
+def reverse_recursive(lst):
+    """Returns the reverse of the given list.
+
+    >>> reverse_recursive([1, 2, 3, 4])
+    [4, 3, 2, 1]
+    """
+    "*** YOUR CODE HERE ***"
+    if len(lst) == 0:
+        return []
+    else:
+        reverse = reverse_recursive(lst[1:])
+        reverse = reverse + [lst[0]]
+        return reverse
+
+
+def distance(city_a, city_b):
+    """
+    >>> city_a = make_city('city_a', 0, 1)
+    >>> city_b = make_city('city_b', 0, 2)
+    >>> distance(city_a, city_b)
+    1.0
+    >>> city_c = make_city('city_c', 6.5, 12)
+    >>> city_d = make_city('city_d', 2.5, 15)
+    >>> distance(city_c, city_d)
+    5.0
+    """
+    "*** YOUR CODE HERE ***"
+    x_a = get_lat(city_a)
+    y_a = get_lon(city_a)
+
+    x_b = get_lat(city_b)
+    y_b = get_lon(city_b)
+
+    return sqrt((x_a - x_b)**2 + (y_a - y_b)**2)
+
+
+def closer_city(lat, lon, city_a, city_b):
+    """
+    Returns the name of either city_a or city_b, whichever is closest to
+    coordinate (lat, lon).
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+    "*** YOUR CODE HERE ***"
+    city = make_city("", lat, lon)
+    d1 = distance(city, city_a)
+    d2 = distance(city, city_b)
+    if d1 < d2:
+        return get_name(city_a)
+    else:
+        return get_name(city_b)
+
+
+def check_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> city_a = make_city('city_a', 0, 1)
+    >>> city_b = make_city('city_b', 0, 2)
+    >>> distance(city_a, city_b)
+    1.0
+    >>> city_c = make_city('city_c', 6.5, 12)
+    >>> city_d = make_city('city_d', 2.5, 15)
+    >>> distance(city_c, city_d)
+    5.0
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    >>> change_abstraction(False)
+    """
+
+
+# Treat all the following code as being behind an abstraction layer, you shouldn't need to look at it!
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return {"name": name, "lat": lat, "lon": lon}
+    else:
+        return [name, lat, lon]
+
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    if change_abstraction.changed:
+        return city["name"]
+    else:
+        return city[0]
+
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    if change_abstraction.changed:
+        return city["lat"]
+    else:
+        return city[1]
+
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return city["lon"]
+    else:
+        return city[2]
+
+
+def change_abstraction(change):
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
+
+
+def add_chars(w1, w2):
+    """
+    Return a string containing the characters you need to add to w1 to get w2.
+
+    You may assume that w1 is a subsequence of w2.
+
+    >>> add_chars("owl", "howl")
+    'h'
+    >>> add_chars("want", "wanton")
+    'on'
+    >>> add_chars("rat", "radiate")
+    'diae'
+    >>> add_chars("a", "prepare")
+    'prepre'
+    >>> add_chars("resin", "recursion")
+    'curo'
+    >>> add_chars("fin", "effusion")
+    'efuso'
+    >>> add_chars("coy", "cacophony")
+    'acphon'
+    """
+    "*** YOUR CODE HERE ***"
+    if w1 == "":
+        return w2
+    elif w2 == "":
+        return w1
+    else:
+        if w1[0] == w2[0]:
+            return add_chars(w1[1:], w2[1:])
+        else:
+            return w2[0] + add_chars(w1, w2[1:])
